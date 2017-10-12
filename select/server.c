293a2ad710e3b03bc90fac8ae4325ac76db8ec7a
Daniel <atomicdanielgadd@hotmail.com>
2017-10-12 18:19:08 +0100

    Select Basic Starting Point - Daniel

--- select/server.c
+++ select/server.c
@@ -26,19 +26,19 @@
 
 int main(int argc, char **argv)
 {
-	int			i;
-	int			max_i;
+	int		i;
+	int		max_i;
 	int 		max_FD;
 	int 		listen_FD;
 	int 		connection_FD;
 	int 		socket_FD;
-	int			nready;
-	int			client[FD_SETSIZE];
+	int		nready;
+	int		client[FD_SETSIZE];
 	
 	ssize_t		n;
 	ssize_t 	FD_set;			
-	ssize_t 	copy_set;
-	ssize_t 	master_set;
+	fd_set 		copy_set;
+	fd_set 		master_set;
 	
 	char 		buffer[1024];
 	
@@ -54,22 +54,22 @@ int main(int argc, char **argv)
 	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	server_addr.sin_port        = htons(1000);
 	
-	//printf("AF_FAMILY : %s", server_addr.sin_family);	//DEBUGGING
-	//printf("ADDRESS : %s", server_addr.sin_addr.s_addr);	//DEBUGGING
-	//printf("PORT : %d", server_addr.sin_port);				//DEBUGGING
+	//printf("AF_FAMILY : %s", server_addr.sin_family); //DEBUGGING
+	//printf("ADDRESS : %s", server_addr.sin_addr.s_addr); //DEBUGGING
+	//printf("PORT : %d", server_addr.sin_port); //DEBUGGING
 
 	bind(listen_FD, (struct sockaddr *) &server_addr, sizeof(server_addr));
 
 	listen(listen_FD, SOMAXCONN);
 
-	max_FD = listen_FD;										// INITIALISE - 1 FD IS STD INPUT - 2 IS STAD OUTPUT - 3 ALSO IS SOMETHING - 4 IS WHATS NOW AVAILABLE AND WE CONTINUE OF THERE - SO WE KNOW 1,2,3 ARE BY DEFAULT TAKEN BY LINUX AND WE MORE THAN LIKELY ARE 4 NOW. THIS IS THE MAX WE HAVE TO CHECK
-	max_i = -1;												// INDEX INTO CLIENT[] ARRAY
+	max_FD = listen_FD; // INITIALISE - 1 FD IS STD INPUT - 2 IS STAD OUTPUT - 3 ALSO IS SOMETHING - 4 IS WHATS NOW AVAILABLE AND WE CONTINUE OF THERE - SO WE KNOW 1,2,3 ARE BY DEFAULT TAKEN BY LINUX AND WE MORE THAN LIKELY ARE 4 NOW. THIS IS THE MAX WE HAVE TO CHECK
+	max_i = -1; // INDEX INTO CLIENT[] ARRAY
 	
-	for (i = 0; i < FD_SETSIZE; i++) { client[i] = -1; } 	// INDICATE AVAILABLE ENTRY
+	for (i = 0; i < FD_SETSIZE; i++) { client[i] = -1; } // INDICATE AVAILABLE ENTRY
 	
-	fd_(&master_set);									//ZERO OUT THE SET
+	FD_ZERO(&master_set); // ZERO OUT THE SET
 	
-	FD_SET(listen_FD, &master_set);							//TELL KERNEL LISTEN TO THIS SET - THE LISTEN FILE DESCRIPTOR
+	FD_SET(listen_FD, &master_set); //TELL KERNEL LISTEN TO THIS SET - THE LISTEN FILE DESCRIPTOR
 
 
 	/////////////////////////////////////////////////////////////////////////////////////
@@ -78,11 +78,11 @@ int main(int argc, char **argv)
 	for ( ; ; ) 
 	{
 	
-		copy_set = master_set;			// STRUCTURE ASSIGNMENT - WE COPY FROM MASTER SET AS NOT TO KEEP A SAFE SET 
+		copy_set = master_set; // STRUCTURE ASSIGNMENT - WE COPY FROM MASTER SET AS NOT TO KEEP A SAFE SET 
 		
-		nready = Select(max_FD + 1, &master_set, NULL, NULL, NULL);
+		nready = select(max_FD + 1, &master_set, NULL, NULL, NULL);
 
-		printf("N READY : %d", nready);//DEBUGGING
+		printf("N READY : %d", nready); //DEBUGGING
 
 
 		///////////////////////////////////////////////////////
@@ -106,15 +106,15 @@ int main(int argc, char **argv)
 				}
 			}
 			
-			if (i == FD_SETSIZE) { err_quit("TOO MANY CLIENTS"); }		//EDN PROGRAM AS TOO MANY CLIENTS
+			if (i == FD_SETSIZE) { printf("TOO MANY CLIENTS"); return 0; } //EDN PROGRAM AS TOO MANY CLIENTS
 
-			FD_SET(connection_FD, &master_set);							// ADD NEW DESCRIPTOR TO SET
+			FD_SET(connection_FD, &master_set); // ADD NEW DESCRIPTOR TO SET
 			
-			if (connection_FD > max_FD) { max_FD = connection_FD; }		// FOR SELECT
+			if (connection_FD > max_FD) { max_FD = connection_FD; } // FOR SELECT
 			
-			if (i > max_i) { max_i = i; }								// MAX INDEX IN CLIENT[] ARRAY
+			if (i > max_i) { max_i = i; } // MAX INDEX IN CLIENT[] ARRAY
 
-			if (--nready <= 0) { continue; }							// NO MORE READABLE DESCRIPTOR
+			if (--nready <= 0) { continue; } // NO MORE READABLE DESCRIPTOR
 		}
 		
 		////////////////////////////////////////////////////////
@@ -125,22 +125,22 @@ int main(int argc, char **argv)
 		////////////////////////////////////////////////////////
 		//CHECK ALL CLIENTS FOR DATA
 		
-		for (i = 0; i <= maxi; i++) 	// CHECK ALL CLIENT FOR DATA
+		for (i = 0; i <= max_i; i++) // CHECK ALL CLIENT FOR DATA
 		{
 			if ( (socket_FD = client[i]) < 0) { continue; }
 				
 			if (FD_ISSET(socket_FD, &copy_set)) 
 			{
-				if ( (n = Read(socket_FD, buffer, MAXLINE)) == 0) 
+				if ( (n = read(socket_FD, buffer, 1024)) == 0) 
 				{	
 					// CONNECTION CLOSED BY CLIENT
-					Close(socket_FD);
+					close(socket_FD);
 					FD_CLR(socket_FD, &master_set);
 					client[i] = -1;
 				} 
-				else{Writen(socket_FD, buffer, n);}
+				else{write(socket_FD, buffer, n);}
 
-				if (--nready <= 0) { break; }							// NO MORE READABLE DESCRIPTION
+				if (--nready <= 0) { break; } // NO MORE READABLE DESCRIPTION
 			}
 		}
 		
